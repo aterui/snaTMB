7c6b326d507fe7da8a635ac1d0d174913591ae6b
AkiraTerui <hanabi0111@gmail.com>
2023-07-08 19:28:02 -0400

    edit: working package

--- R/core_semtmb.R
+++ R/core_semtmb.R
@@ -1,12 +1,33 @@
 #' Fit a spatial error model (SEM) with TMB
 #'
-#' @param x Adjacency matrix to be converted
-#'
+#' @param formula an object of class "formula" (or one that can be coerced to that class): a symbolic description of the model to be fitted.
+#' @param data an optional data frame, list or environment (or object coercible by as.data.frame to a data frame) containing the variables in the model. If not found in data, the variables are taken from environment(formula).
 #' @author Akira Terui, \email{hanabi0111@gmail.com}
-#'
+#' @useDynLib semtmb
 #' @export
 #'
 
-semtmb <- function() {
+semtmb <- function(formula,
+                   data) {
+
+  y <- model.frame(formula, data)[,1]
+  X <- model.matrix(formula, data)
+
+  obj <- TMB::MakeADFun(data = list(y = y,
+                                    D = D,
+                                    X = X),
+                        parameters = list(b = rep(0, ncol(X)),
+                                          log_sigma = log(0.1),
+                                          log_theta = log(10)),
+                        DLL = "semtmb",
+                        silent = TRUE)
+
+  opt <- nlminb(start = obj$par,
+                obj = obj$fn,
+                gr = obj$gr)
+
+  rep <- TMB::sdreport(obj)
+
+  return(summary(rep, "report", p.value = TRUE))
 
 }
--- tools/build.R
+++ tools/build.R
@@ -25,4 +25,27 @@ devtools::check(vignettes=FALSE)
 #usethis::use_pkgdown()
 
 # Run to build the website
-pkgdown::build_site()
+# pkgdown::build_site()
+
+
+# -------------------------------------------------------------------------
+
+library(dplyr)
+set.seed(121)
+N <- 200
+theta <- 10
+eps <- rnorm(N, sd = 0.1)
+X <- cbind(1, rnorm(N), rnorm(N))
+beta <- c(0.01, 0.8, 0.2)
+
+D <- cbind(runif(N, 0, 1000), runif(N, 0, 1000)) %>%
+  dist(diag = TRUE, upper = TRUE) %>%
+  data.matrix()
+S <- 0.1 * exp(-D / theta)
+
+y_hat <- X %*% beta
+y <- mvtnorm::rmvnorm(1, mean = y_hat, sigma = S) %>% c()
+
+df0 <- dplyr::tibble(x1 = X[,2], x2 = X[,3], y = y)
+
+semtmb(y ~ x1 + x2, data = df0)
